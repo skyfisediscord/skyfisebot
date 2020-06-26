54ccc4a84f3de03fb1bbf81ccba3e7031bb2a664
SkYFiSe <60897388+SkYFiSe@users.noreply.github.com>
2020-06-26 23:34:20 +0300

    Update index.js
    update token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("NzI1NDYzMzM3MTgxMjQ5NTg2.XvW8CQ.t7m7qK3xtfzfPxi6iImJCetdAY0");
+client.login("NzI1NDYzMzM3MTgxMjQ5NTg2.XvZZaA.sb3M5YESTnPEHd48wmENtrKbot8");
 
 client.on('message', (message) =>{
     if(message.content == "&test")
